221d7bded5a25366f75adaf7bc4bb8e9efa99dd7
VictorDotZ <victor.zenin.o@gmail.com>
2022-04-20 15:15:16 +0300

    apply elementwise fixed; extend comparisons replace by decorator

--- 03/custom_list.py
+++ 03/custom_list.py
@@ -1,5 +1,16 @@
+from functools import total_ordering
+
+from utils import apply_operator
+
+
+@total_ordering
 class CustomList(list):
-    def __init__(self, data=None):
+    __le__ = object.__le__
+    __ge__ = object.__ge__
+    __gt__ = object.__gt__
+    __ne__ = object.__ne__
+
+    def __init__(self, data: list):
         super().__init__()
         self._data = data
 
@@ -9,9 +20,6 @@ class CustomList(list):
     def __getitem__(self, i):
         return self._data[i]
 
-    def __setitem__(self, i, value):
-        self._data[i] = value
-
     def __str__(self):
         return f"{sum(self._data)}:\t{str(self._data)}"
 
@@ -19,14 +27,14 @@ class CustomList(list):
         out = []
         min_len = min(len(self), len(other))
         for i in range(min_len):
-            if operator == "+":
-                out.append(self[i] + other[i])
-            elif operator == "-":
-                out.append(self[i] - other[i])
+            out.append(apply_operator(self[i], other[i], operator))
+
+        if len(self) > len(other):
+            out += [apply_operator(x, 0, operator) for x in self[min_len:]]
+
         if len(self) < len(other):
-            out += other[min_len:]
-        else:
-            out += self[min_len:]
+            out += [apply_operator(0, x, operator) for x in other[min_len:]]
+
         return CustomList(out)
 
     def __add__(self, other):
@@ -38,17 +46,5 @@ class CustomList(list):
     def __lt__(self, other):
         return sum(self[0:]) < sum(other[0:])
 
-    def __le__(self, other):
-        return not self > other
-
     def __eq__(self, other):
         return sum(self[0:]) == sum(other[0:])
-
-    def __ne__(self, other):
-        return not self == other
-
-    def __gt__(self, other):
-        return sum(self[0:]) > sum(other[0:])
-
-    def __ge__(self, other):
-        return not self < other
---
+++ 03/utils.py
@@ -0,0 +1,6 @@
+def apply_operator(num1, num2, operator="+"):
+    if operator == "+":
+        return num1 + num2
+    if operator == "-":
+        return num1 - num2
+    return None
